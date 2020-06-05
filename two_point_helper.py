6f1b674b0d2d85b8188936a24960d29e9122e315
Nitish <jhanitishkumar12@gmail.com>
2020-06-05 15:04:34 +0530

    finding mid point

---
+++ two_point_helper.py
@@ -0,0 +1,14 @@
+import math
+
+###################################
+######  work in progress   ########
+###################################
+
+# point passed be list,tuple
+
+
+def get_mid_point(point1,point2):
+    mid_point_x = (point1[0]+point2[0])/2
+    mid_point_y = (point1[1]+point2[1])/2
+    return [mid_point_x, mid_point_y]
+
