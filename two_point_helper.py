ca404f4f36977eba5e1c79d9269ffe4ad8eff490
Nitish <jhanitishkumar12@gmail.com>
2020-06-17 19:30:49 +0530

    angle between 2 points in 3d

--- two_point_helper.py
+++ two_point_helper.py
@@ -7,20 +7,22 @@ from numpy.linalg import lstsq
 ######  work in progress   ########
 ###################################
 
-# point passed be list,tuple
+# point passed be in list,tuple
 
 
-def mid_point_between_points(point1,point2):
+def mid_point_between_points(point1, point2):
     mid_point_x = (point1[0]+point2[0])/2
     mid_point_y = (point1[1]+point2[1])/2
     return [mid_point_x, mid_point_y]
 
-def slope_between_points(point1,point2):
+
+def slope_between_points(point1, point2):
     if point1[0] == point2[0]:
         return np.inf
     else:
         m = (point2[1] - point1[1]) / (point2[0] - point1[0])
         return m
+
 
 def distance_between_points(point1, point2,refrance_axis):
     if refrance_axis == "x":
@@ -30,10 +32,21 @@ def distance_between_points(point1, point2,refrance_axis):
     if refrance_axis == "xy":
         return math.sqrt((point1[0]-point2[0])**2+(point1[1]-point2[1])**2)
 
+
 def equation_of_line(point1,point2):
 
-    points = [point1,point2]
+    points = [point1, point2]
     x_coords, y_coords = zip(*points)
     A = vstack([x_coords,ones(len(x_coords))]).T
     m, c = lstsq(A, y_coords)[0]
     return "y={m}x + {c}".format(m=m, c=c), m, c
+
+
+def angle_3d_points(point1, point2):
+    "cos(theta) = a.b/|a||b|"
+
+    ab = (point1[0] * point2[0]) + (point1[1] * point2[1]) + (point1[2] * point2[2])
+    mod_a = math.sqrt(point1[0] ** 2 + point1[1] ** 2 + point1[2] ** 2)
+    mod_b = math.sqrt(point2[0] ** 2 + point2[1] ** 2 + point2[2] ** 2)
+    angle = math.acos(ab / (mod_a * mod_b))
+    return angle
